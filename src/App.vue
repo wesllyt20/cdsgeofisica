c0d7d7daecf23f33dd30f1546e2d312a95fd830c
wesllyt20 <raphael.09@hotmail.es>
2023-03-31 17:08:17 -0500

    Primer Commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <viewPrincipal/>
+    </v-main>
+  </v-app>
+
+
+  <!--
+
+  <v-btn id="btnFlecha" class="rail-variant ml-n6" :class="{ 'rail-variant--open': drawer }" icon @click.stop="drawer = !drawer">
+      <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16.7071 6.29289C16.3166 5.90237 15.6834 5.90237 15.2929 6.29289C14.9024 6.68342 14.9024 7.31658 15.2929 7.70711L18.5858 11L3 11C2.44772 11 2 11.4477 2 12C2 12.5523 2.44772 13 3 13L18.5858 13L15.2929 16.2929C14.9024 16.6834 14.9024 17.3166 15.2929 17.7071C15.6834 18.0976 16.3166 18.0976 16.7071 17.7071L21.7071 12.7071C22.0976 12.3166 22.0976 11.6834 21.7071 11.2929L16.7071 6.29289Z" fill="#000000"/>
+      </svg>
+    </v-btn>
+
+  -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import viewPrincipal from './components/viewPrincipal.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    viewPrincipal,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/viewPrincipal.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-app>
+    <div class="barra-superior"></div>
+    <div class="barra-inferior">
+      <v-btn text>Portal IGP</v-btn>
+      <v-btn text>Servicios</v-btn>
+      <v-btn text>Infraestructura de datos espaciales (IDE - IGP)</v-btn>
+      <v-btn text>Visor Geográfico</v-btn>
+      <v-btn text>CDSGeofísico</v-btn>
+    </div>
+
+    <v-btn id="btnFlecha" class="rail-variant ml-n6" :class="{ 'rail-variant--open': drawer }" icon @click.stop="drawer = !drawer">
+    <v-icon>mdi-menu</v-icon>
+    </v-btn>
+    <v-navigation-drawer v-model="drawer">
+      <h4>Hola</h4>
+    </v-navigation-drawer>
+
+    <div id="contenedor" class="fill-height">
+      <iframe id="inlineFrameExample" src="https://ide.igp.gob.pe/geovisor/estudios-cts/"></iframe>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      railVariantLeft: 10
+    };
+  },
+  watch: {
+    drawer(val) {
+      this.railVariantLeft = val ? 260 : 10;
+    }
+  }
+};
+</script>
+
+<style scoped>
+#contenedor {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  border: none;
+  z-index: 1;
+}
+
+#inlineFrameExample {
+  width: 100%;
+  height: 100%;
+  border: none;
+}
+
+.fill-height {
+  height: 100vh;
+}
+
+.rail-variant {
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  transition: left 0.3s ease-in-out;
+}
+
+.rail-variant--open {
+  left: 260px;
+}
+#btnFlecha{
+  margin-top: 90px;
+  position: absolute;
+   z-index: 999; 
+}
+.barra-superior {
+  height: 80px;
+  background-color: #000;
+}
+
+</style>
